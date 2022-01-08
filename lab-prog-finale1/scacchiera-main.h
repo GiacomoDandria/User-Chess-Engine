8d1e47692b398209dd5bb3b15cf7b3e56ac39232
GiacomoDandria <73328482+GiacomoDandria@users.noreply.github.com>
2022-01-08 19:53:16 +0100

    cambiato il nome della classe contenente il main in scacchiera-main e aggiunto file scacchiera-main.h

---
+++ lab-prog-finale1/scacchiera-main.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include<vector>
+#include<limits>
+#include<iostream>
+#include"traduttore.h"
+#include "scacchiera.h"
+#include "pedone.h"
+#include "alfiere.h"
+#include "regina.h"
+#include "re.h"
+#include "cavallo.h"
+#include "torre.h"
+#include"computer.h"
+#include "scacco.h"
+
+int main();
