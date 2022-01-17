f43d9d511fbf2f35fa3d24697d8d7324a3570410
GiacomoDandria <73328482+GiacomoDandria@users.noreply.github.com>
2022-01-17 19:17:36 +0100

    commenti middlePieces.cpp

--- lab-prog-finale1/src/middlePieces.cpp
+++ lab-prog-finale1/src/middlePieces.cpp
@@ -1,46 +1,34 @@
 #include "middlePieces.h"
 
-/*TODO: - finire funzioni di check*/
-
-/*controllo che le caselle attraverso le quali una pediana si vuole muovere siano effettivamente vuote*/
-
 bool middlePieces::check(scacchiera& board, int fromLetter, int fromNumber, int toLetter, int toNumber) {
 	//controllo che le posizioni della scacchiera tra quella di partenza e quella di arrivo siano vuote
 
-	//caso 1: lettera di partenza uguale a quella di arrivo: movimento in verticale--FUNZIONA
+	//caso 1: lettera di partenza uguale a quella di arrivo: movimento in verticale
 	if (fromLetter == toLetter) {
 		//caso 1.1: movimento verticale verso il basso
 		if (fromNumber < toNumber) {
 			for (auto i = fromNumber + 1; i < toNumber; i++) {
-			//DEBUG TOGLI PER LA BELLA
-			//std::cout << "asfoadifjsioad: " << board.getPiece(i, fromLetter) << std::endl;
 				if (board.getPiece(i, fromLetter) != 0x20) {
-					//std::cout << "fromletter==toletter-verticale-verso-basso" << std::endl;
 					return false;
 				}	
 			}
-			//return true; //AGGIUNTO DA LOSQUARCIO PER DEBUG13
 		}
 		//caso 1.2: movimento verticale verso l'alto
 		else if (fromNumber > toNumber) {
 			for (auto i = fromNumber - 1; i > toNumber; i--) {
-			//std::cout << "asfoadifjsioad: " << board.getPiece(i, fromLetter) << std::endl;
 				if (board.getPiece(i, fromLetter) != 0x20) {
-					//std::cout << "fromletter==toletter-verticale-verso-alto" << std::endl;
 					return false;
 				}
 			}
 		}
 	}
 	
-	//caso 2: numero di partenza uguale a quello di arrivo: movimento in orizzontale--FUNZIONA
+	//caso 2: numero di partenza uguale a quello di arrivo: movimento in orizzontale
 	if (fromNumber == toNumber) {
 		//caso 2.1: movimento orizzontale verso destra
 		if (fromLetter < toLetter) {
 			for (auto i = fromLetter + 1; i < toLetter; i++) {
-			//std::cout << "asfoadifjsioad: " << board.getPiece(fromNumber, i) << std::endl;
 				if (board.getPiece(fromNumber, i) != 0x20) {
-					//std::cout << "fromnumber==tonumber-verso-destra" << std::endl;
 						return false;
 				}
 			}
@@ -48,80 +36,48 @@ bool middlePieces::check(scacchiera& board, int fromLetter, int fromNumber, int 
 		//caso 2.2: movimento orizzontale verso sinistra
 		else if (fromLetter > toLetter) {
 			for (auto i = fromLetter - 1; i > toLetter; i--) {
-			//std::cout << "asfoadifjsioad: " << board.getPiece(fromNumber, i) << std::endl;
 				if (board.getPiece(fromNumber, i) != 0x20) {
-					//std::cout << "fromnumber==tonumber-verso-sinistra" << std::endl;
 						return false;
 				}
 			}
 		}
 	}
 	
-	//caso 3: numero di spostamenti nella direzione verticale è uguale a quello in orizzontale: movimento obliquo
+	//caso 3: numero di spostamenti nella direzione verticale Ã¨ uguale a quello in orizzontale: movimento obliquo
 	if (std::abs(toLetter - fromLetter) == std::abs(toNumber - fromNumber)) {
-
-		//caso 3.1: movimento nella direzione in basso-destra--FUNZIONA
+		//caso 3.1: movimento nella direzione in basso-destra
 		if (toLetter > fromLetter && toNumber > fromNumber) {
 			int temp = fromNumber + 1;
 			for (auto i = fromLetter + 1; i < toLetter; i++) {
 				fromNumber += 1;
-				//std::cout << "asofiajfioa: " << board.getPiece(fromNumber, i) << std::endl;
 				if (board.getPiece(fromNumber, i) != 0x20) {
-					//std::cout << "toLetter > fromLetter && toNumber > fromNumber" << std::endl;
 					return false;
 				}
 			}
 		}
-		//caso 3.2: movimento in direzione in alto-destra--FUNZIONA
+		//caso 3.2: movimento in direzione in alto-destra
 		if (toLetter > fromLetter && toNumber < fromNumber) {
-			//std::cout << "dasofijasofjiasofjasoifjaisdioasjdiaso" << std::endl;
-			//int temp = fromNumber;
-			/*std::cout << "From letter: " << fromLetter << std::endl;
-			std::cout << "From number: " << fromNumber << std::endl;
-			std::cout << "To letter: " << toLetter << std::endl;
-			std::cout << "To number: " << toNumber << std::endl;*/
-
-
 			for (auto i = fromLetter + 1; i < toLetter; i++) {
 				fromNumber -= 1;
-				//std::cout << "asofiajfioa: " << board.getPiece(fromNumber, i) << std::endl;
 				if (board.getPiece(fromNumber, i) != 0x20) {
-					//std::cout << "toLetter > fromLetter && toNumber < fromNumber" << std::endl;
 					return false;
 				}
 			}
 		}
-		//caso 3.3: movimento in direzione in basso-sinistra--FUNZIONA
+		//caso 3.3: movimento in direzione in basso-sinistra
 		if (toLetter < fromLetter && toNumber > fromNumber) {
-			//std::cout << "dasofijasofjiasofjasoifjaisdioasjdiaso" << std::endl;
-			////int temp = fromNumber;
-			//std::cout << "From letter: " << fromLetter << std::endl;
-			//std::cout << "From number: " << fromNumber << std::endl;
-			//std::cout << "To letter: " << toLetter << std::endl;
-			//std::cout << "To number: " << toNumber << std::endl;
 			for (auto i = fromLetter - 1; i > toLetter; i--) {
 				fromNumber += 1;
-
-				//std::cout << "asofiajfioa: " << board.getPiece(fromNumber, i) << std::endl;
 				if (board.getPiece(fromNumber, i) != 0x20) {
-					//std::cout << "toLetter < fromLetter && toNumber > fromNumber" << std::endl;
 					return false;
 				}
 			}
 		}
-		//caso 3.4: movimento in direzione in alto-sinistra--FUNZIONA
+		//caso 3.4: movimento in direzione in alto-sinistra
 		if (toLetter < fromLetter && toNumber < fromNumber) {
-			//std::cout << "dasofijasofjiasofjasoifjaisdioasjdiaso" << std::endl;
-			////int temp = fromNumber;
-			//std::cout << "From letter: " << fromLetter << std::endl;
-			//std::cout << "From number: " << fromNumber << std::endl;
-			//std::cout << "To letter: " << toLetter << std::endl;
-			//std::cout << "To number: " << toNumber << std::endl;
 			for (auto i = fromLetter - 1; i > toLetter; i--) {
 				fromNumber -= 1;
-				//std::cout << "asofiajfioa: " << board.getPiece(fromNumber, i) << std::endl;
 				if (board.getPiece(fromNumber, i) != 0x20) {
-					//std::cout << "toLetter < fromLetter && toNumber < fromNumber" << std::endl;
 					return false;
 				}
 			}
@@ -129,4 +85,3 @@ bool middlePieces::check(scacchiera& board, int fromLetter, int fromNumber, int 
 	}
 	return true;
 }
-
