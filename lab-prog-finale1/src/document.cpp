107851314f375de63233b1ae1dca89d4602e42eb
GiacomoDandria <73328482+GiacomoDandria@users.noreply.github.com>
2022-01-17 19:12:53 +0100

    commenti document.cpp

--- lab-prog-finale1/src/document.cpp
+++ lab-prog-finale1/src/document.cpp
@@ -1,9 +1,9 @@
 #include "document.h"
 
+//aggiunge una stringa (arg) passata come argomento della funzione al file passato
+//anch'esso come argomento (title)
 void document::add_line(std::string title, std::string arg) {
 	std::fstream file;
-	//file.open(title, std::ios::out);
-	//file.close();
 	file.open(title, std::ios::app);
 	if (file.is_open()) {
 		file << arg << std::endl;
