5d3d46ba8fce16090d42f06b17d9bec868daed66
GiacomoDandria <73328482+GiacomoDandria@users.noreply.github.com>
2022-01-17 19:39:21 +0100

    commeti torre.cpp

--- lab-prog-finale1/src/torre.cpp
+++ lab-prog-finale1/src/torre.cpp
@@ -1,12 +1,12 @@
 #include "torre.h"
 
-/*NB: il movimento delle pedine maiuscole o minuscole non cambia, perché la torre può muoversi
-		  sia verticalmente che orizzontalmente senza costrizioni*/
 bool torre::moveTorreCheck(std::string inputMovement) {
 	//traduzione degli input da "giocatore umano" a indici array 2D
 	std::vector<int> input = traduttore::traduci(inputMovement);
 
-	//movimento verticale o orizzontale
+	//movimento verticale o orizzontale:
+	//rispettivamente, o la lettera rimane costante e varia il numero, oppure il numero
+	//rimane costante e la lettera varia
 	if ((input.at(0) == input.at(2) && input.at(1) != input.at(3)) || (input.at(1) == input.at(3) && input.at(0) != input.at(2)))
 		return true;
 
