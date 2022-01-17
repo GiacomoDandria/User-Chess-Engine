9a1a5c75964b15dfbd19ecae13e12b24c71e1752
GiacomoDandria <73328482+GiacomoDandria@users.noreply.github.com>
2022-01-17 19:28:19 +0100

    commenti replay.cpp

--- lab-prog-finale1/src/replay.cpp
+++ lab-prog-finale1/src/replay.cpp
@@ -1,6 +1,5 @@
 #include "replay.h"
 
-
 using namespace std;
 
 int main(int argCount, char* argVec[])
@@ -17,24 +16,28 @@ int main(int argCount, char* argVec[])
         return 1;
     }
 
+    //creo una scacchiera su su eseguire le mosse (che si assume saranno sicuramente corrette,
+    //in quanto replay di una partita gia' effettuata in passato, dunque non vengono fatti
+    //controlli sulla validita' delle mosse, ma solo su quella delle stringhe di input)
     scacchiera board;
-
     fstream fileReader;
-//---------------DA CAMBIARE IN argVec[0] il path del file!!!!!!!!!!!!!!!!!!!!!!!!!!!
     fileReader.open(argVec[1], ios::in);
     if (fileReader.is_open()) {
         string myText;
-        while (getline(fileReader, myText)) { //read data from file object and put it into string.
-            //cout << (string)myText << endl;
-
+        
+        //leggo tutto il file e, per ogni linea, traduco l'input e svolgo la mossa sulla scacchiera
+        while (getline(fileReader, myText)) {
             std::vector<int> input = traduttore::traduci(myText);
             board.movePedina(input.at(0), input.at(1), input.at(2), input.at(3));
+            
+            //a seconda della presenza o meno di un secondo parametro passato a riga di comando
+            //la scacchiera viene stampata a video piuttosto che su un altro file
             if (argCount == 2) {
              board.printScacchieraVideo();
+             }
+            if (argCount == 3) {
+             board.printScacchieraFile(argVec[2]);
          }
-        if (argCount == 3) {
-         board.printScacchieraFile(argVec[2]);
-     }
     }
         fileReader.close();
     }       	 
