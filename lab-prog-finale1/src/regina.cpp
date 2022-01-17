4dd6918f2cb55a2c180c81cce411910fb8cc8a8d
GiacomoDandria <73328482+GiacomoDandria@users.noreply.github.com>
2022-01-17 19:23:54 +0100

    commenti regina.cpp

--- lab-prog-finale1/src/regina.cpp
+++ lab-prog-finale1/src/regina.cpp
@@ -1,12 +1,14 @@
 #include "regina.h"
 
-/*TODO: - controllo tramite getPiece che le posizioni della scacchiera tra la posizione d'inizio e di arrivo siano vuoti*/
-
 bool regina::moveReginaCheck(std::string inputMovement) {
 	//traduzione degli input da "giocatore umano" a indici array 2D
 	std::vector<int> input = traduttore::traduci(inputMovement);
 
-	//il movimento della regina è composto da quello dell'alfiere o quello della torre - ma non entrambi contemporaneamente
+	//il movimento della regina e' composto da quello dell'alfiere o quello della torre - ma non entrambi contemporaneamente
+	//dunque, nei diversi casi, bisogna controllare che:
+	//	- la lettera rimanga la stessa e cambi solo il numero -> movimento verticale
+	//	- il numero rimanga lo stesso e cambi solo la lettera -> movimento orizzontale
+	//	- lettera e numero cambino, in valore assoluto, della stessa quantita' -> moviemnto obliquo
 	if ((std::abs(input.at(2) - input.at(0)) == std::abs(input.at(3) - input.at(1))) ||
 		(input.at(0) == input.at(2) && input.at(1) != input.at(3)) || (input.at(1) == input.at(3) && input.at(0) != input.at(2))) {
 		return true;
@@ -14,4 +16,3 @@ bool regina::moveReginaCheck(std::string inputMovement) {
 
 	return false;
 }
-
