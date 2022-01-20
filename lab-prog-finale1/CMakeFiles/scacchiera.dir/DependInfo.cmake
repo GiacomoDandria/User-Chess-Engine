f8ebf800fcd36660ec9147b7939f461b5bc0e28f
GiovanniBellato <96789638+GiovanniBellato@users.noreply.github.com>
2022-01-20 19:31:19 +0100

    git ignore

---
+++ lab-prog-finale1/CMakeFiles/scacchiera.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/alfiere.cpp" "CMakeFiles/scacchiera.dir/src/alfiere.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/alfiere.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/cavallo.cpp" "CMakeFiles/scacchiera.dir/src/cavallo.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/cavallo.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/computer.cpp" "CMakeFiles/scacchiera.dir/src/computer.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/computer.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/document.cpp" "CMakeFiles/scacchiera.dir/src/document.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/document.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/middlePieces.cpp" "CMakeFiles/scacchiera.dir/src/middlePieces.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/middlePieces.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/randomMoves.cpp" "CMakeFiles/scacchiera.dir/src/randomMoves.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/randomMoves.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/re.cpp" "CMakeFiles/scacchiera.dir/src/re.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/re.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/regina.cpp" "CMakeFiles/scacchiera.dir/src/regina.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/regina.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacchiera-main.cpp" "CMakeFiles/scacchiera.dir/src/scacchiera-main.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/scacchiera-main.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacchiera.cpp" "CMakeFiles/scacchiera.dir/src/scacchiera.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/scacchiera.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacco.cpp" "CMakeFiles/scacchiera.dir/src/scacco.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/scacco.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/torre.cpp" "CMakeFiles/scacchiera.dir/src/torre.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/torre.cpp.o.d"
+  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/traduttore.cpp" "CMakeFiles/scacchiera.dir/src/traduttore.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/traduttore.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab-prog-finale1/CMakeFiles/scacchiera.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scacchiera.dir/src/alfiere.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/alfiere.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/cavallo.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/cavallo.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/computer.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/computer.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/document.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/document.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/middlePieces.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/middlePieces.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/randomMoves.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/randomMoves.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/re.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/re.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/regina.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/regina.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/scacchiera-main.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/scacchiera-main.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/scacchiera.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/scacchiera.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/scacco.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/scacco.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/torre.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/torre.cpp.o.d"
+  "CMakeFiles/scacchiera.dir/src/traduttore.cpp.o"
+  "CMakeFiles/scacchiera.dir/src/traduttore.cpp.o.d"
+  "scacchiera"
+  "scacchiera.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scacchiera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
