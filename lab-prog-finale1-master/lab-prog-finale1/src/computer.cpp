4b4b58122894d22694f9e81ce2d281472ae2dc6a
LOSquarcio <96296566+LOSquarcio@users.noreply.github.com>
2022-01-21 18:58:55 +0100

    Commenti compuetercpp

--- lab-prog-finale1-master/lab-prog-finale1/src/computer.cpp
+++ lab-prog-finale1-master/lab-prog-finale1/src/computer.cpp
@@ -1,8 +1,15 @@
+//AUTORE: MARCO SQUARCINA
+
 #include"computer.h"
-
-computer::computer(){throw NoCorrectInfoConstructor();}   //costruttore vuoto
-
-computer::computer(char c) {                       //costruttore pedine white o black
+//COSTRUTTORI
+computer::computer(){                           
+    throw NoCorrectInfoConstructor();                       //Se viene chiamato il costruttore senza argomenti lancia eccezione
+}     
+/*Questo costruttore tiene conto del colore delle pedine del giocatore. Serve per inizializzare il vector
+  in modo tale che contenga le coordinate appropriate. 
+  Queste coordinate verranno estratte e utilizzate per eseguire le mosse random. Saranno rimosse le coordinate delle pedine mangiate
+  e aggiornate quelle delle pedine mosse. (SERVE AL COMPUTER PER SAPERE COSA "PUO'" MUOVERE).  */
+computer::computer(char c) {                                
     //se bianche
     if (c == 'w')
     {
@@ -50,62 +57,66 @@ computer::computer(char c) {                       //costruttore pedine white o 
     
     
 }
-
+/*Restituisce la dimensione della lista. Questo valore verrà utilizzato per avere un indice per utilizzare
+  la funzione random, che agira' sugli elementi del vector.  */
 int computer::getSize() {
     int n = (int)computer::coordinate.size();
     return n;
 }
 
-//funzione automossa
+/* FUNZIONE per far muovere random una pedina al computer. 
+   Muovo random la pedina con l'uso della scacchiera; il flag diventa true quando la mossa e' stata effettuata
+   correttamente. In caso contrario continua a cercare di muovere fino a che non muove una pedina.     */
+
 std::string computer::autoMove(scacchiera & board1) {
     bool flag = true;
     std::vector <int> move;
-    /*muovo random la pedina con l'uso della scacchiera
-      il flag diventa true quando la mossa e' stata effettuata
-      correttamente.In caso contrario continua a cercare di muovere fino a che 
-      non muove una pedina. */
+    
 
-    int n = (int)computer::coordinate.size(); //dimensione del vector per estrarre random le coordinate
+    int n = (int)computer::coordinate.size();                   //dimensione del vector per estrarre random le coordinate
     std::string s1 = "";
     std::string s2 = "";
+    
+    //Continuo il ciclo fino a quando non trovo una mossa valida
     do
     {
         
-        srand((unsigned)time(0));           //scelgo random una pedina dal vector
+        srand((unsigned)time(0));                                 //scelgo random una pedina dal vector
         int r = std::rand() % (n);
-        s1 = computer::coordinate.at(r);
-        std::vector<int> temp = traduttore::traduci(s1+" "+s1);
-        char type = board1.getPiece(temp.at(1), temp.at(2));
+        s1 = computer::coordinate.at(r);                          //coordinate di partenza
+        std::vector<int> temp = traduttore::traduci(s1+" "+s1);   //vector temporaneo per passare s1 alla funzione randomMoves
+        char type = board1.getPiece(temp.at(1), temp.at(2));      //restituice il char relativo alla pedina che si sta per muovere
         
-        s2 = randomMove(board1, s1, type);
+        s2 = randomMove(board1, s1, type);                        //chiamo la funzione randomMoves, la quale resituisce una posizione valida per la mossa              
         std::string daTradurre = s1 + " " + s2;
-        move = traduttore::traduci(daTradurre);
+        move = traduttore::traduci(daTradurre);                   //traduco nel vector move le coordinate da stringa a intero per lavorare meglio con le matrici
         
-        if (middlePieces::check(board1, move.at(0), move.at(1), move.at(2), move.at(3)))
+        if (middlePieces::check(board1, move.at(0), move.at(1), move.at(2), move.at(3)))        //controllo pezzi tra la posizione di partenza e quella di arrivo
         {
-            if (board1.movePedina(move.at(0), move.at(1), move.at(2), move.at(3))) //muovo la pedina 
+            if (board1.movePedina(move.at(0), move.at(1), move.at(2), move.at(3)))              //muovo la pedina 
             {
-                flag = false;
-                document::add_line("log.txt", daTradurre);
-                computer::coordinate[r] = s2; //aggiorno posizione pedina nel vector
+                flag = false;                                //mossa effettuata
+                document::add_line("log.txt", daTradurre);   //mossa inserita nel file log    
+                computer::coordinate[r] = s2;                //aggiorno posizione nel vector
             }
         }
-    } while (flag);
+    } while (flag);    
     
-    return s1+" "+s2;
+    return s1+" "+s2;       //ritorno le coordinate della mossa
 }
 
-//restituisce una posizione random all'interno della scacchiera
+/*Questo e' il primo tentativo di fare una funzione che desse una posizione random all'interno della scacchiera per muovere una pedina. A
+  causa della poca efficenza abbiamo creato un ulteriore funzione semi random, presente nel file randomMoves.cpp . */
 std::string randomPosition(std::string s) {
-    char letter = 0;
-    char number = 0;
+    char letter = 0;               //conterra' la lettera di arrivo
+    char number = 0;               //conterra' il numero di arrivo
     std::string position = "";
 
     srand((unsigned)time(0));
-    letter = (std::rand() % 8) +1;
+    letter = (std::rand() % 8) +1;          //scelgo random una lettera
     srand((unsigned)time(0));
-    number = (std::rand() % 8) +1;
-    //seleziono la lettera
+    number = (std::rand() % 8) +1;          //scelgo random una colonna
+    //Converto letter e number in una stringa con le coordinate appropriate
     if (letter == 1)
         position = "A" + (std::to_string(number));
     else if (letter == 2)
@@ -122,16 +133,18 @@ std::string randomPosition(std::string s) {
         position = "G" + (std::to_string(number));
     else if (letter == 8)
         position = "H" + (std::to_string(number));
+    
     //ritorno la stringa che contiene una coordinata RANDOM
-
     return position;
 }
 
-//funzione remove piece
+/*FUNZIONE remove piece.
+  Riceve una stringa con le coordinate di dove e' andata una pedina avversaria. Se in quelle coordinate c'era una pedina
+  della classe corrente questa sara' rimossa dal vector (Ovvero e' stata mangiata).*/
 void computer::removePiece(const std::string s)
 {
-    std::string str = s.substr(3, 2);
-    if (std::find(computer::coordinate.begin(), computer::coordinate.end(), str) != computer::coordinate.end()) {
+    std::string str = s.substr(3, 2);       //coordinate di arrivo
+    if (std::find(computer::coordinate.begin(), computer::coordinate.end(), str) != computer::coordinate.end()) {       //se e' presente rimuovo
            computer::coordinate.erase(std::find(computer::coordinate.begin(), computer::coordinate.end(), str));
         
     }
