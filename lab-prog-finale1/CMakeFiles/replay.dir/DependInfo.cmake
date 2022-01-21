54105fd98cbb49b0d966f0267f5fa8481159f6d0
GiovanniBellato <96789638+GiovanniBellato@users.noreply.github.com>
2022-01-21 10:36:18 +0100

    git ignore

--- lab-prog-finale1/CMakeFiles/replay.dir/DependInfo.cmake
+++ lab-prog-finale1/CMakeFiles/replay.dir/DependInfo.cmake
@@ -1,22 +1,24 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/alfiere.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/alfiere.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/cavallo.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/cavallo.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/document.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/document.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/re.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/re.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/regina.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/regina.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/replay.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/replay.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacchiera.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/scacchiera.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/torre.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/torre.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/traduttore.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/replay.dir/src/traduttore.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/alfiere.cpp" "CMakeFiles/replay.dir/src/alfiere.cpp.o" "gcc" "CMakeFiles/replay.dir/src/alfiere.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/cavallo.cpp" "CMakeFiles/replay.dir/src/cavallo.cpp.o" "gcc" "CMakeFiles/replay.dir/src/cavallo.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/document.cpp" "CMakeFiles/replay.dir/src/document.cpp.o" "gcc" "CMakeFiles/replay.dir/src/document.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/re.cpp" "CMakeFiles/replay.dir/src/re.cpp.o" "gcc" "CMakeFiles/replay.dir/src/re.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/regina.cpp" "CMakeFiles/replay.dir/src/regina.cpp.o" "gcc" "CMakeFiles/replay.dir/src/regina.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/replay.cpp" "CMakeFiles/replay.dir/src/replay.cpp.o" "gcc" "CMakeFiles/replay.dir/src/replay.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacchiera.cpp" "CMakeFiles/replay.dir/src/scacchiera.cpp.o" "gcc" "CMakeFiles/replay.dir/src/scacchiera.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/torre.cpp" "CMakeFiles/replay.dir/src/torre.cpp.o" "gcc" "CMakeFiles/replay.dir/src/torre.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/traduttore.cpp" "CMakeFiles/replay.dir/src/traduttore.cpp.o" "gcc" "CMakeFiles/replay.dir/src/traduttore.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "h"
   )
 
 # Targets to which this target links.
--- lab-prog-finale1/CMakeFiles/scacchiera.dir/DependInfo.cmake
+++ lab-prog-finale1/CMakeFiles/scacchiera.dir/DependInfo.cmake
@@ -1,26 +1,28 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/alfiere.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/alfiere.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/cavallo.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/cavallo.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/computer.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/computer.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/document.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/document.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/middlePieces.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/middlePieces.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/randomMoves.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/randomMoves.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/re.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/re.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/regina.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/regina.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacchiera-main.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/scacchiera-main.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacchiera.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/scacchiera.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacco.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/scacco.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/torre.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/torre.cpp.o"
+  "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/src/traduttore.cpp" "/Users/bellato/Documents/GitHub/lab-prog-finale1/lab-prog-finale1/CMakeFiles/scacchiera.dir/src/traduttore.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/alfiere.cpp" "CMakeFiles/scacchiera.dir/src/alfiere.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/alfiere.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/cavallo.cpp" "CMakeFiles/scacchiera.dir/src/cavallo.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/cavallo.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/computer.cpp" "CMakeFiles/scacchiera.dir/src/computer.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/computer.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/document.cpp" "CMakeFiles/scacchiera.dir/src/document.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/document.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/middlePieces.cpp" "CMakeFiles/scacchiera.dir/src/middlePieces.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/middlePieces.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/randomMoves.cpp" "CMakeFiles/scacchiera.dir/src/randomMoves.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/randomMoves.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/re.cpp" "CMakeFiles/scacchiera.dir/src/re.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/re.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/regina.cpp" "CMakeFiles/scacchiera.dir/src/regina.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/regina.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacchiera-main.cpp" "CMakeFiles/scacchiera.dir/src/scacchiera-main.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/scacchiera-main.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacchiera.cpp" "CMakeFiles/scacchiera.dir/src/scacchiera.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/scacchiera.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/scacco.cpp" "CMakeFiles/scacchiera.dir/src/scacco.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/scacco.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/torre.cpp" "CMakeFiles/scacchiera.dir/src/torre.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/torre.cpp.o.d"
-  "/Users/gio/GitHub/lab-prog-finale1/lab-prog-finale1/src/traduttore.cpp" "CMakeFiles/scacchiera.dir/src/traduttore.cpp.o" "gcc" "CMakeFiles/scacchiera.dir/src/traduttore.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "h"
   )
 
 # Targets to which this target links.
