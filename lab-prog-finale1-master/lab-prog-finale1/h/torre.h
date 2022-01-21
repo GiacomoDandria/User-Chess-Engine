5307da3820f7bd168da097bc32588d67c8045e6a
LOSquarcio <96296566+LOSquarcio@users.noreply.github.com>
2022-01-21 19:40:26 +0100

    COmmenti torre.h

--- lab-prog-finale1-master/lab-prog-finale1/h/torre.h
+++ lab-prog-finale1-master/lab-prog-finale1/h/torre.h
@@ -1,9 +1,12 @@
+
+//AUTORE: MARCO SQUARCINA
+
 #pragma once
 #include<string>
 #include "scacchiera.h"
 #include "traduttore.h"
 
 struct torre {
-	static bool moveTorreCheck(std::string inputMovement);
+	static bool moveTorreCheck(std::string inputMovement); //Controllo validita' mossa della torre
 };
 
