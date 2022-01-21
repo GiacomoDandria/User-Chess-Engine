5cab9d3793a6b82946c0b975daaa3422b30e2e21
LOSquarcio <96296566+LOSquarcio@users.noreply.github.com>
2022-01-21 19:08:29 +0100

    Commento randomMoves.h

--- lab-prog-finale1-master/lab-prog-finale1/h/randomMoves.h
+++ lab-prog-finale1-master/lab-prog-finale1/h/randomMoves.h
@@ -1,3 +1,5 @@
+//AUTORE: MARCO SQUARCINA
+
 #include <iostream>
 #include <random>
 #include <string>
@@ -8,16 +10,14 @@
 #include "traduttore.h"
 #include "middlePieces.h"
 
-std::string randomMove(scacchiera & ,std::string, char);
+std::string randomMove(scacchiera & ,std::string, char);    //Ritorna delle coordinate random data una certa pedina
 
-bool rangeCheck(int colonna, int riga);
+bool rangeCheck(int colonna, int riga);                     //Controllo coordinate dentro i range della scacchiera
 
-bool friendlyCheckU(scacchiera&, int colonna, int riga);
+bool friendlyCheckU(scacchiera&, int colonna, int riga);    //Controllo se la pedina , date le coordinate, e' maiuscola
+                                                            // U sta per UpperCase
+bool friendlyCheckL(scacchiera&, int colonna, int riga);    //Controllo se la pedina , date le coordinate, e' minuscola 
+                                                            //L sta per LowerCase
+bool someone(scacchiera&, int colonna, int riga);           //Controlla se e' presente una pedina date certe coordinate 
 
-bool friendlyCheckL(scacchiera&, int colonna, int riga);
-
-bool someone(scacchiera&, int colonna, int riga);
-
-
-
-std::string traduzionePerRandom(int colonna, int riga);
+std::string traduzionePerRandom(int colonna, int riga);     //E' una traduzione piu' specifica da coordinate a stringa
