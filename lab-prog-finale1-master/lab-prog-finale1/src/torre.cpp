a56ad01d3d06a7571b77a6ce19d4141add58eac0
LOSquarcio <96296566+LOSquarcio@users.noreply.github.com>
2022-01-21 19:42:53 +0100

    commenti torre.cpp

--- lab-prog-finale1-master/lab-prog-finale1/src/torre.cpp
+++ lab-prog-finale1-master/lab-prog-finale1/src/torre.cpp
@@ -1,12 +1,15 @@
+
+//AUTORE: MARCO SQUARCINA
+
 #include "torre.h"
 
 bool torre::moveTorreCheck(std::string inputMovement) {
-	//traduzione degli input da "giocatore umano" a indici array 2D
-	std::vector<int> input = traduttore::traduci(inputMovement);
+	
+	std::vector<int> input = traduttore::traduci(inputMovement);   //traduzione degli input da "giocatore umano" a indici matrice
 
-	//movimento verticale o orizzontale:
-	//rispettivamente, o la lettera rimane costante e varia il numero, oppure il numero
-	//rimane costante e la lettera varia
+	/*Controllo se sono eseguite dei movimenti validi ovvero: verticale e orrizzontale. 
+	  -Stessa riga ma varia la colonna (ORRIZZONTALE)
+	  -Stessa colonna ma varia la riga (VERTICALE)    */
 	if ((input.at(0) == input.at(2) && input.at(1) != input.at(3)) || (input.at(1) == input.at(3) && input.at(0) != input.at(2)))
 		return true;
 
