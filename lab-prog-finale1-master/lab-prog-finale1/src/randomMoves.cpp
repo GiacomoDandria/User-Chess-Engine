19e45f09bd68dda07e8a7449405738b649786ff0
LOSquarcio <96296566+LOSquarcio@users.noreply.github.com>
2022-01-21 19:27:51 +0100

    randomMoves comment
    Nel primo cavallo ho ridotto il controllo if a uno solo. (CHECK)

--- lab-prog-finale1-master/lab-prog-finale1/src/randomMoves.cpp
+++ lab-prog-finale1-master/lab-prog-finale1/src/randomMoves.cpp
@@ -1,142 +1,133 @@
+//AUTORE: MARCO SQUARCINA
+
 #include "randomMoves.h"
 
+/*Questa funzione permette date le coordinate di partenza e il tipo di carattere, di restituire delle coordinate di 
+  arrivo valide. E' semi random poiche' la scelta della pedina e' completamente random, mentre la scelta della posizione
+  viene eseguita per tentativi mediante dei cicli while. Questo permette di velocizzare molto le giocate del computer.
+  Vengono eseguiti diversi tipi di controlli: Range, Non venga mangiata una pedina dello stesso colore, non si vada "fuori"
+  dalla scacchiera.  */
 std::string randomMove(scacchiera& board, std::string position, char piece) {
 
-	std::vector <int> move = traduttore::traduci(position + " " + position);
-	std::vector <int> random;
+	std::vector <int> move = traduttore::traduci(position + " " + position);  //vector contenente coordinate di partenza (in stile matrice)
+	std::vector <int> random;						  //vector contenente coordinate di arrivo (in stile matrice)
 	int colonna = 0;
 	int riga = 0;
-	std::string pos = "";
-
+	std::string pos = "";							  //stringa per le coordinate di arrivo (pos = posizione)
+	
+	//Ogni pedina ha il suo case (controllo mosse valide)
+	//Viene descritto solo il primo blocco di codice poiche' e' ripetitivo
 	switch (piece)
 	{
 	//---CAVALLO----
 	case('c') : {
-		int count = 8;
-		while (count > 0) {
-			//#1
-			if (count == 8) {
-				colonna = move.at(0) - 2;
-				riga = move.at(1) - 1;
-				if (rangeCheck(colonna, riga))
-					if (friendlyCheckL(board, colonna, riga))
-						return traduzionePerRandom(colonna, riga);
+		int count = 8;		//numero max mosse della pedina data
+		while (count > 0) {	//ciclo per trovare mossa valida
+			//Controllo #1
+			if (count == 8) {		
+				colonna = move.at(0) - 2;					//Inserisco le coordinate del caso in questione
+				riga = move.at(1) - 1;	
 			}
-			//#2
+			//Controllo #2
 			if (count == 7) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) - 2;
-				if (rangeCheck(colonna, riga))
-					if (friendlyCheckL(board, colonna, riga))
-						return traduzionePerRandom(colonna, riga);
 			}
-			//#3
+			//Controllo #3
 			if (count == 6) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) - 2;
-				if (rangeCheck(colonna, riga))
-					if (friendlyCheckL(board, colonna, riga))
-						return traduzionePerRandom(colonna, riga);
 			}
-			//#4
+			//Controllo#4
 			if (count == 5) {
 				colonna = move.at(0) + 2;
 				riga = move.at(1) - 1;
-				if (rangeCheck(colonna, riga))
-					if (friendlyCheckL(board, colonna, riga))
-						return traduzionePerRandom(colonna, riga);
 			}
-			//#5
+			//Controllo #5
 			if (count == 4) {
 				colonna = move.at(0) + 2;
 				riga = move.at(1) + 1;
-				if (rangeCheck(colonna, riga))
-					if (friendlyCheckL(board, colonna, riga))
-						return traduzionePerRandom(colonna, riga);
 			}
-			//#6
+			//Controllo #6
 			if (count == 3) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) + 2;
-				if (rangeCheck(colonna, riga))
-					if (friendlyCheckL(board, colonna, riga))
-						return traduzionePerRandom(colonna, riga);
 			}
-			//#7
+			//Controllo #7
 			if (count == 2) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) + 2;
-				if (rangeCheck(colonna, riga))
-					if (friendlyCheckL(board, colonna, riga))
-						return traduzionePerRandom(colonna, riga);
 			}
-			//#8
+			//Controllo #8
 			else if (count == 1) {
 				colonna = move.at(0) - 2;
 				riga = move.at(1) + 1;
-				if (rangeCheck(colonna, riga))
-					if (friendlyCheckL(board, colonna, riga))
-						return traduzionePerRandom(colonna, riga);
 			}
-
-			//end ciclo
+			//Controllo validita' coordinate
+			if (rangeCheck(colonna, riga))					//Controllo se il range e' rispettato
+				if (friendlyCheckL(board, colonna, riga))		//Controllo se ci sono pedine dello stesso colore
+					return traduzionePerRandom(colonna, riga);	//Ritorno la stringa contenente le coordinate valide
+			//Fine ciclo
 			count--;
 		}
+		//Se non sono state trovate delle coordinate valide restituisco quelle di partenza; verra' segnalato
+		//errore quando si tentera' la mossa
 		break;
 	}
 	case('C') : {
 		int count = 8;
 		while (count > 0) {
-			//#1
+			//Controllo #1
 			if (count == 3) {
 				colonna = move.at(0) - 2;
 				riga = move.at(1) - 1;
 			}
-			//#2
+			//Controllo #2
 			if (count == 1) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) - 2;
 			}
-			//#3
+			//Controllo #3
 			if (count == 2) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) - 2;
 			}
-			//#4
+			//Controllo #4
 			if (count ==4 ) {
 				colonna = move.at(0) + 2;
 				riga = move.at(1) - 1;
 			}
-			//#5
+			//Controllo #5
 			if (count == 6) {
 				colonna = move.at(0) + 2;
 				riga = move.at(1) + 1;
 			}
-			//#6
+			//Controllo #6
 			if (count == 8) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) + 2;
 			}
-			//#7
+			//Controllo #7
 			if (count == 7) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) + 2;
 			}
-			//#8
+			//Controllo #8
 			else if (count == 5) {
 				colonna = move.at(0) - 2;
 				riga = move.at(1) + 1;
 			}
-			//controllo movimento
+			//Controllo se le coordinate di arrivo sono valide. In caso contrario ripeto il ciclo
 			if (rangeCheck(colonna, riga))
 				if (friendlyCheckU(board, colonna, riga))
 					return traduzionePerRandom(colonna, riga);
-			//end ciclo
+			//Fine ciclo
 			count--;
 		}
 		break;
 	}
 	//----PEDONE----
+	//Il pedone ha un controllo in piu', poiche' non pue' procedere se nelle diagonali c'e' una pedina (func. someone)
 	case('p') : {
 		int count = 3;
 		while (count > 0) {
@@ -209,42 +200,42 @@ std::string randomMove(scacchiera& board, std::string position, char piece) {
 	case('d') : case('r'):{
 		int count = 8;
 		while (count > 0) {
-			//#1
+			//Controllo #1
 			if (count == 6) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) - 1;
 			}
-			//#2
+			//Controllo #2
 			if (count == 8) {
 				colonna = move.at(0);
 				riga = move.at(1) - 1;
 			}
-			//#3
+			//Controllo #3
 			if (count == 5) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) - 1;
 			}
-			//#4
+			//Controllo #4
 			if (count == 7) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1);
 			}
-			//#5
+			//Controllo #5
 			if (count == 3) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) + 1;
 			}
-			//#6
+			//Controllo #6
 			if (count == 1) {
 				colonna = move.at(0);
 				riga = move.at(1) + 1;
 			}
-			//#7
+			//Controllo #7
 			if (count == 2) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) + 1;
 			}
-			//#8
+			//Controllo #8
 			if (count == 4) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1);
@@ -262,42 +253,42 @@ std::string randomMove(scacchiera& board, std::string position, char piece) {
 	case ('D'):case('R'): {
 		int count = 8;
 		while (count > 0) {
-			//#1
+			//Controllo #1
 			if (count == 3) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) - 1;
 			}
-			//#2
+			//Controllo #2
 			if (count == 1) {
 				colonna = move.at(0);
 				riga = move.at(1) - 1;
 			}
-			//#3
+			//Controllo #3
 			if (count == 4) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) - 1;
 			}
-			//#4
+			//Controllo #4
 			if (count == 2) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1);
 			}
-			//#5
+			//Controllo #5
 			if (count == 6) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) + 1;
 			}
-			//#6
+			//Controllo #6
 			if (count == 8) {
 				colonna = move.at(0);
 				riga = move.at(1) + 1;
 			}
-			//#7
+			//Controllo #7
 			if (count == 7) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) + 1;
 			}
-			//#8
+			//Controllo #8
 			if (count == 5) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1);
@@ -316,27 +307,27 @@ std::string randomMove(scacchiera& board, std::string position, char piece) {
 	case('a') : {
 		int count = 4;
 		while (count > 0) {
-			//#1
+			//Controllo #1
 			if (count == 4) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) - 1;
 			}
-			//#2
+			//Controllo #2
 			if (count == 2) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) + 1;
 			}
-			//#3
+			//Controllo #3
 			if (count == 3) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) - 1;
 			}
-			//#4
+			//Controllo #4
 			else if (count == 1) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) + 1;
 			}
-			//controllo move
+			//controllo movement
 			if (rangeCheck(colonna, riga)) {
 				if (friendlyCheckL(board, colonna, riga))
 						return traduzionePerRandom(colonna, riga);
@@ -348,27 +339,27 @@ std::string randomMove(scacchiera& board, std::string position, char piece) {
 	case('A'): {
 		int count = 4;
 		while (count > 0) {
-			//#1
+			//Controllo #1
 			if (count == 1) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) - 1;
 			}
-			//#2
+			//Controllo #2
 			if (count == 3) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) + 1;
 			}
-			//#3
+			//Controllo #3
 			if (count == 2) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1) - 1;
 			}
-			//#4
+			//Controllo #4
 			else if (count == 4) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1) + 1;
 			}
-			//controllo move
+			//controllo movement
 			if (rangeCheck(colonna, riga)) {
 				if (friendlyCheckU(board, colonna, riga))
 					return traduzionePerRandom(colonna, riga);
@@ -381,27 +372,27 @@ std::string randomMove(scacchiera& board, std::string position, char piece) {
 	case('t'): {
 		int count = 4;
 		while (count > 0) {
-			//#1
+			//Controllo #1
 			if (count == 4) {
 				colonna = move.at(0) ;
 				riga = move.at(1) - 1;
 			}
-			//#2
+			//Controllo #2
 			if (count == 3) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1);
 			}
-			//#3
+			//Controllo #3
 			if (count == 2) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1);
 			}
-			//#4
+			//Controllo #4
 			else if (count == 1) {
 				colonna = move.at(0);
 				riga = move.at(1) + 1;
 			}
-			//controllo move
+			//controllo movement
 			if (rangeCheck(colonna, riga)) {
 				if (friendlyCheckL(board, colonna, riga))
 						return traduzionePerRandom(colonna, riga);
@@ -413,27 +404,27 @@ std::string randomMove(scacchiera& board, std::string position, char piece) {
 	case ('T'): {
 		int count = 4;
 		while (count > 0) {
-			//#1
+			//Controllo #1
 			if (count == 1) {
 				colonna = move.at(0);
 				riga = move.at(1) - 1;
 			}
-			//#2
+			//Controllo #2
 			if (count == 2) {
 				colonna = move.at(0) + 1;
 				riga = move.at(1);
 			}
-			//#3
+			//Controllo #3
 			if (count == 3) {
 				colonna = move.at(0) - 1;
 				riga = move.at(1);
 			}
-			//#4
+			//Controllo #4
 			else if (count == 4) {
 				colonna = move.at(0);
 				riga = move.at(1) + 1;
 			}
-			//controllo move
+			//controllo movement
 			if (rangeCheck(colonna, riga)) {
 				if (friendlyCheckU(board, colonna, riga))
 					return traduzionePerRandom(colonna, riga);
@@ -443,12 +434,13 @@ std::string randomMove(scacchiera& board, std::string position, char piece) {
 		break;
 	}
 	}
+	//Se non ho trovato coordinate valide ritorno la posizione di partenza
 	return position;
 }
 
 //----------------------------------------------------------------
 //----RANGE_CHECK-----
-//Per non uscire dalle coordinate consentite
+//Controllo per non uscire dalle coordinate consentite dalla scacchiera
 bool rangeCheck(int colonna, int riga) {
 	if (colonna < 0 || colonna>7)
 		return false;
@@ -458,8 +450,8 @@ bool rangeCheck(int colonna, int riga) {
 }
 //-----FRIENDLY_MOVES_UPPERCASE-----
   /*Contolla se la casella di arrivo e' gia' occupata da una 
-	pedina dello stesso colore. Se e' dello stesso colore ritorna 
-	false  */
+    pedina dello stesso colore. Se e' dello stesso colore ritorna 
+    false  */
 
 bool friendlyCheckU(scacchiera& board, int colonna, int riga) {
 	char piece = board.getPiece(riga, colonna);
@@ -473,8 +465,8 @@ bool friendlyCheckU(scacchiera& board, int colonna, int riga) {
 }
 //-----FRIENDLY_MOVES_LOWERCASE-----
 /*Contolla se la casella di arrivo e' gia' occupata da una 
-	pedina dello stesso colore. Se e' dello stesso colore ritorna 
-	false  */
+  pedina dello stesso colore. Se e' dello stesso colore ritorna 
+  false  */
 bool friendlyCheckL(scacchiera& board, int colonna, int riga) {
 	char piece = board.getPiece(riga, colonna);
 
@@ -486,6 +478,7 @@ bool friendlyCheckL(scacchiera& board, int colonna, int riga) {
 	return true;
 }
 //----TRADUZIONE_PER_RANDOM----
+//Traduce interi corrispondenti a riga e colonna della matrice in una stringa a livello giocatore
 std::string traduzionePerRandom(int colonna, int riga) {
 	std::string pos = "";
 	pos = traduttore::traduciReverse(colonna, riga, colonna, riga);
